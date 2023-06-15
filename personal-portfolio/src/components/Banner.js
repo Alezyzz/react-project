6aa2913ce23c282cbd8da2303130e52940b569df
Alezyzz <alexander.bjornas@gmail.com>
2023-06-15 12:56:03 +0200

    Fixed banner

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -6,7 +6,7 @@ import headerImg from "../assets/img/header-img.svg"
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Web Developer", "Test"];
+    const toRotate = ["Web Developer", "Test", "Test2"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -22,7 +22,7 @@ export const Banner = () => {
     const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text-lengt -1) : fullText.substring(0, text.length + 1);
+        let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
 
         setText(updatedText);
 
@@ -46,7 +46,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{'Hi Im Alex'}<span className="wrap">{text}</span></h1>
+                        <h1>{'Hi Im Alex '}<span className="wrap">{text}</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida sem nibh, at accumsan diam posuere quis. Ut sagittis viverra consectetur. Aenean nec metus massa. Nam vitae congue turpis. </p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
